d8b1650fcb05f0e9bf6bdfb06952972e80ba10ff
Pablo Huinac <pablohuinac@hotmail.com>
2016-09-30 10:10:01 -0600

    Redireccion de publicaciones

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from .models import Postear
 
 def post_list(request):
 	posts = Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
-	return render(request, 'blog/post_list.html',{'posts': posts})
+	return render(request, 'blog/listar_articulos.html',{'posts': posts})
 
 def post_detail(request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        return render(request, 'blog/post_detail.html', {'post': post})
+        post = get_object_or_404(Postear, pk=pk)
+        return render(request, 'blog/detalle_articulo.html', {'Postear': post})
